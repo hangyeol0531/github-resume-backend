6517d1a8d0a47802f2ff6042e10b317a324994d2
hangyeol0531 <jhan0531@naver.com>
2023-04-15 14:03:31 +0900

    refactor/allowCacheMethods 분리

--- src/interceptors/http-cache.interceptor.ts
+++ src/interceptors/http-cache.interceptor.ts
@@ -2,9 +2,11 @@ import { ExecutionContext, Injectable, CacheInterceptor } from '@nestjs/common';
 
 @Injectable()
 export class HttpCacheInterceptor extends CacheInterceptor {
+  private allowCacheMethods = ['GET'];
+
   trackBy(context: ExecutionContext): string | undefined {
     const { method, url } = context.switchToHttp().getRequest();
-    if (method !== 'GET') return;
-    return url;
+    if (this.allowCacheMethods.includes(method)) return url;
+    return null;
   }
 }
