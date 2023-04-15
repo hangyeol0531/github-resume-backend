4592440365e822f7a050d15f0ea16c5bb7dc1a2e
hangyeol0531 <jhan0531@naver.com>
2023-04-15 22:25:42 +0900

    chore/add, modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,8 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'import/prefer-default-export': 'off',
-    "class-methods-use-this": "off",
+    'class-methods-use-this': 'off',
+    'max-classes-per-file': 'off',
     'import/no-extraneous-dependencies':[
       'error',
       {
