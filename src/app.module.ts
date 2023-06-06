658167be87e7d9f6556bba40e70d37df83107909
hangyeol0531 <jhan0531@naver.com>
2023-06-06 19:36:27 +0900

    build/add redis env log

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,12 +26,20 @@ import redisConfig from './config/redisConfig';
       imports: [ConfigModule],
       inject: [ConfigService],
       isGlobal: true,
-      useFactory: (configService: ConfigService) => ({
-        store: redisStore,
-        host: configService.get('REDIS_HOST'),
-        port: configService.get('REDIS_PORT'),
-        ttl: 60 * 60,
-      }),
+      useFactory: (configService: ConfigService) => {
+        console.log(
+          `========== host : ${configService.get('REDIS_HOST')} =======`,
+        );
+        console.log(
+          `========== port : ${configService.get('REDIS_PORT')} =======`,
+        );
+        return {
+          store: redisStore,
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+          ttl: 60 * 60,
+        };
+      },
     }),
   ],
   controllers: [AppController],
