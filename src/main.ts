40836b27703e4c7a9afeb448f976f6bd1071d351
hangyeol0531 <jhan0531@naver.com>
2023-06-06 19:19:37 +0900

    build/ docker port mount 변경, add new deploy check log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { useSwagger } from './swagger';
 
 async function bootstrap() {
+  console.log('==================== new deploy ===============');
   const app = await NestFactory.create(AppModule);
   // TODO cors 임시 전체 허용
   app.enableCors();
