021d9207832a4fd1ac7e1d8b5c4efc46150dd484
hangyeol0531 <jhan0531@naver.com>
2023-06-10 14:41:32 +0900

    feature/add healthCheck

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/health')
+  @ApiOperation({ summary: 'healthCheck' })
+  async healthCheck() {
+    return true;
+  }
 }
