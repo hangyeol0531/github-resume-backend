50ed21668d652b59dce42e96130c971c1b391e3f
hangyeol0531 <jhan0531@naver.com>
2023-06-11 23:37:49 +0900

    feature/add userId validation

--- src/github/github.message.ts
+++ src/github/github.message.ts
@@ -1,3 +1,5 @@
 export enum GithubMessage {
+  NOT_EXISTS_USER_ID = '유저 아이디가 존재하지 않습니다.',
+  NOT_VALID_USER_ID = '유저 아이디 형식이 올바르지 않습니다.',
   NOT_FOUND_USER = '유저가 존재하지 않습니다.',
 }
--- src/github/github.user.validation.pipe.ts
+++ src/github/github.user.validation.pipe.ts
@@ -4,12 +4,18 @@ import {
   Injectable,
   PipeTransform,
 } from '@nestjs/common';
+import { GithubMessage } from './github.message';
 
 @Injectable()
 export class GithubUserValidationPipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata) {
     if (!value || value === ':userId') {
-      throw new BadRequestException('유저 아이디가 올바르지 않습니다.');
+      throw new BadRequestException(GithubMessage.NOT_EXISTS_USER_ID);
+    }
+
+    const userIdValidationRegex = /^[a-zA-Z\d]([-a-zA-Z\d]){0,38}$/;
+    if (!userIdValidationRegex.test(value)) {
+      throw new BadRequestException(GithubMessage.NOT_VALID_USER_ID);
     }
     return value;
   }
