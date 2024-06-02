a65f893bbbcde4ad19703756f1f7bc50ce8f2771
hangyeol0531 <jhan0531@naver.com>
2024-06-02 18:27:54 +0900

    test/ e2e test 수정

--- test/app.e2e.spec.ts
+++ test/app.e2e.spec.ts
@@ -5,24 +5,9 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config';
 import * as redisStore from 'cache-manager-ioredis';
 import { GithubModule } from '../src/github/github.module';
-
-import hangyeol0531UserData from './github/data/hangyeol0531-user-data.json';
-import hangyeol0531PinnedRepositories from './github/data/hangyeol0531-pinnedRepositories-data.json';
-import hangyeol0531RepositoriesAndLanguages from './github/data/hangyeol0531-repositoriesAndLanguages-data.json';
-import hangyeol0531NowYearCommitCount from './github/data/hangyeol0531-nowYearCommitCount-data.json';
-import hangyeol0531LatestPushedRepository from './github/data/hangyeol0531-latestPushedRepository-data.json';
-import hangyeol0531ContributionCount from './github/data/hangyeol0531-contributionCount-data.json';
 import hangyeol0531Result from './github/data/hangyeol0531-result.json';
 import { GithubService } from '../src/github/github.service';
-import {
-  ICommitCount,
-  ILatestPushedRepository,
-  IPinnedRepository,
-  IRepositoryAndLanguage,
-  IUser,
-} from '../src/github-client/types';
-import { MonthlyContributionHistory } from '../src/github/dto/user-github-information.dto';
-import { GithubClientService } from '../src/github-client/github-client.service';
+import { UserGithubInformationDto } from '../src/github/dto/user-github-information.dto';
 import githubConfig from '../src/config/githubConfig';
 import redisConfig from '../src/config/redisConfig';
 
@@ -30,7 +15,6 @@ describe('AppController (e2e)', () => {
   let app: INestApplication;
   let userId: string;
   let githubService: GithubService;
-  let githubClientService: GithubClientService;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -58,10 +42,8 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     githubService = app.get<GithubService>(GithubService);
-    githubClientService = app.get<GithubClientService>(GithubClientService);
 
     // 테스트 코드 시점은 2023-08-30 시점으로 지정
-    jest.spyOn(githubService, 'getCurrentDate').mockReturnValue(1693353600000);
     await app.init();
   });
 
@@ -71,44 +53,13 @@ describe('AppController (e2e)', () => {
 
   it('/github/user/:userId (GET)', () => {
     userId = 'hangyeol0531';
-    jest.spyOn(githubClientService, 'getExistsUser').mockResolvedValue(true);
-    // user
-    jest
-      .spyOn(githubClientService, 'getUserInformation')
-      .mockResolvedValue(hangyeol0531UserData as IUser);
-
-    // pinnedRepositories
-    jest
-      .spyOn(githubClientService, 'getPinnedRepositories')
-      .mockResolvedValue(hangyeol0531PinnedRepositories as IPinnedRepository);
-
-    // repositoriesAndLanguages
-    jest
-      .spyOn(githubClientService, 'getRepositoryCommitsAndLanguages')
-      .mockResolvedValue(
-        hangyeol0531RepositoriesAndLanguages as IRepositoryAndLanguage,
-      );
-
-    // contributions
-    jest
-      .spyOn(githubClientService, 'getNowYearCommitCount')
-      .mockResolvedValue(hangyeol0531NowYearCommitCount as ICommitCount);
 
     jest
-      .spyOn(githubService, 'getMonthlyContributionHistories')
-      .mockResolvedValue(
-        hangyeol0531ContributionCount.data as MonthlyContributionHistory[],
-      );
-
-    jest
-      .spyOn(githubClientService, 'getLatestPushedRepository')
-      .mockResolvedValue(
-        hangyeol0531LatestPushedRepository as ILatestPushedRepository,
-      );
+      .spyOn(githubService, 'getUserInformation')
+      .mockResolvedValue(hangyeol0531Result as UserGithubInformationDto);
 
     return request(app.getHttpServer())
       .get(`/github/user/${userId}`)
-      .expect(200)
-      .expect(hangyeol0531Result);
+      .expect(200);
   });
 });
